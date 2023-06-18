1d6add84b547c0b0310f6a13aed550d5f09984eb
bashiroo2 <130142411+bashiroo2@users.noreply.github.com>
2023-06-19 00:25:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Movie App with React Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
